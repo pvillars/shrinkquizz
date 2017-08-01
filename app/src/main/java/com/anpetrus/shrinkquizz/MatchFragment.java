effa820cf180756f3dac3d09cd8740084ecff44e
Petrus <pvillar@hotmail.cl>
2017-07-31 22:32:08 -0400

    Seek bar finalizado

--- app/src/main/java/com/anpetrus/shrinkquizz/MatchFragment.java
+++ app/src/main/java/com/anpetrus/shrinkquizz/MatchFragment.java
@@ -2,10 +2,14 @@ package com.anpetrus.shrinkquizz;
 
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 
 /**
@@ -30,4 +34,57 @@ public class MatchFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_match, container, false);
     }
 
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        final SeekBar userSb = view.findViewById(R.id.userSb);
+        final TextView userTv = view.findViewById(R.id.userTv);
+        final SeekBar loverSb = view.findViewById(R.id.loverSb);
+        final TextView loverTv = view.findViewById(R.id.loverTv);
+
+        Button button = view.findViewById(R.id.matchBtn);
+
+        userSb.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean b) {
+                userTv.setText(String.valueOf(progress));
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+        loverSb.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean b) {
+                loverTv.setText(String.valueOf(progress));
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int user = userSb.getProgress();
+                int lover = loverSb.getProgress();
+            }
+        });
+    }
 }
